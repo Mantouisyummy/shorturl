7ee8fb6073f53edcce763a595a6c38a2bc951893
Mantou <51238168+Mantou-9487@users.noreply.github.com>
2023-12-26 21:12:44 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 import uvicorn
+import sys
+import subprocess
+
+command = [
+    sys.executable,
+    '-m',
+    'pip',
+    'install',
+    '--requirement',
+    'requirements.txt',
+]
+
+subprocess.check_call(command)
 
 uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", debug=True,
                 workers=1, limit_concurrency=1, limit_max_requests=1)
